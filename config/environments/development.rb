93172a8d4da91dbfad4f39bd43f1e122ccaed18a
irvisdevai <irvisdevai@gmail.com>
2026-04-02 00:07:35 +0000

    remove comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,6 @@ Rails.application.configure do
   port = ENV.fetch("APP_PORT", "3000")
 
   url_options = { host: host }
-  # Solo agregamos el puerto a la URL si NO es el puerto web por defecto
   url_options[:port] = port unless %w[80 443].include?(port.to_s)
 
   config.action_mailer.default_url_options = url_options
