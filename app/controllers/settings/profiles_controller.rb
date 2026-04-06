a480a9163a7fb8d5efeda2d498a37af00d65c065
Matt Szein <70776023+mattszein@users.noreply.github.com>
2026-04-06 18:34:47 -0300

    Simplify profile creation in set_profile method
    Refactor set_profile method to simplify profile creation logic.

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -21,14 +21,6 @@ module Settings
 
     def set_profile
       @profile = current_account.profile
-      return if @profile
-
-      begin
-        @profile = current_account.create_profile!
-      rescue ActiveRecord::RecordNotUnique
-        current_account.reload
-        @profile = current_account.profile
-      end
     end
 
     def profile_params
