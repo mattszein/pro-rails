a3660da951be39b833e8434fc515ab1770c1881b
Matias Szeinfeld <mattszeindev@gmail.com>
2026-04-09 10:48:25 -0300

    support focus in sidebar navigation

--- app/components/core/sidebar_link_component.rb
+++ app/components/core/sidebar_link_component.rb
@@ -21,15 +21,15 @@ class Core::SidebarLinkComponent < ApplicationViewComponent
   ANIMATED_ICON_ACTION = "mouseenter->animated-icon#start mouseleave->animated-icon#stop focusin->animated-icon#start focusout->animated-icon#stop"
 
   def link_classes
-    "flex gap-2 items-center p-3 hover:cursor-pointer rounded-sm w-full group/link"
+    "flex gap-2 items-center p-3 hover:cursor-pointer rounded-sm w-full group/link focus:outline-none"
   end
 
   def icon_classes
-    class_names("h-8 text-gray-500 dark:text-gray-400 group-hover/link:text-secondary-500 dark:group-hover/link:text-secondary-500", ICON_STYLES[active?])
+    class_names("h-8 text-gray-500 dark:text-gray-400 group-hover/link:text-secondary-500 dark:group-hover/link:text-secondary-500 group-focus/link:text-secondary-500 dark:group-focus/link:text-secondary-500", ICON_STYLES[active?])
   end
 
   def span_classes
-    class_names("opacity-100 ml-2 text-lg group-hover/drawer:opacity-100 group-hover/drawer:transition-opacity group-hover/link:text-primary-500 dark:group-hover/link:text-primary-500", SPAN_STYLES[active?])
+    class_names("opacity-100 ml-2 text-lg group-hover/drawer:opacity-100 group-hover/drawer:transition-opacity group-hover/link:text-primary-500 dark:group-hover/link:text-primary-500 outline-none group-focus/link:text-primary-500 dark:group-focus/link:text-primary-500", SPAN_STYLES[active?])
   end
 
   def link_html_options
